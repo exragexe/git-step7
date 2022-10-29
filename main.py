28c867f664bfc71adc963013686d5fce6b8374a7
yaros <yaroslavdisiak@gmail.com>
2022-10-30 00:01:29 +0300

    lab 3 done

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+
+try:
+    text = input("Ведіть текст:")
+    znak = input("Введіть знак який ви шукаєте:")
+    def check (text, znak):
+        if not text:
+            return 0
+        elif text[0] == znak:
+            return 1 + check(text[1:], znak)
+        else:
+            return check(text[1:], znak)
+
+    print(f"Кількість {znak} в тексті - {check(text, znak)}")
+
+except Exception as e:
+    print(e)
