4d150c5366f6ec71909cd128d059816927a22df3
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-29 13:37:04 +0300

    lab 2 done

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+
+try:
+    user = input("-> ")
+    numbers = 0
+    for sign in user:
+        if sign.isdigit():
+            numbers += 1
+    print(f"Цифри:{numbers}, Букви:{len(user)}")
+except Exception as e:
+    print(e)
