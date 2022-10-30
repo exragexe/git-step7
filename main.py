3d21f5067756f0485d9567c57b580e2bfa01e188
yaros <yaroslavdisiak@gmail.com>
2022-10-30 11:03:14 +0200

    lab 4 done

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+
+try:
+    text = input("Введіть текст: ")
+    slow = input("Введіть слово яке ви шукаєте: ")
+
+    search = text.count(slow)
+    print(f"Кількість {slow} у тексті - {search}")
+
+
+except Exception as e:
+    print(e)
+
