d9766a58ec972cf6d2c5e77fa7b7229e326d12ef
yaros <yaroslavdisiak@gmail.com>
2022-10-30 11:55:46 +0200

    hm 1 done

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+
+try:
+    user = input("Введіть рядок: ")
+    if user[::1] == user[::-1]:
+        print(f"{user} - є паліндромом")
+    else:
+        print("не є паліндромом")
+
+
+except Exception as e:
+    print(e)
