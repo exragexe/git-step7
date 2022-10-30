73b70b31f390ff7124bba5bd20af05f95055625f
yaros <yaroslavdisiak@gmail.com>
2022-10-30 17:46:54 +0200

    HM 3 done

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+
+try:
+    text = "The majority of the victims were Jews, but many other people were killed as well, \n including Poles" \
+           " Gypsies, Soviet prisoners of war and tens of thousands of men and women who were killed because they were gay."
+    print(text)
+    str_text = text.split(" ")
+
+    print(len(str_text)-1) #віднімаю 1 через те що спліт добавляє \n в масив і лен його бачить а це не частина тексту
+
+
+except Exception as e:
+    print(e)
