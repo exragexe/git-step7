a2a4216b731bb3a220a814a197802e8c386861c6
yaros <yaroslavdisiak@gmail.com>
2022-10-30 17:12:51 +0200

    HM 2 done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 
+
+try:
+    text = input("Введіть текст: ")
+    znak = input("Введіть слово: ")
+
+    if znak not in text:
+        print("Слова немає в тексті")
+    else:
+        res = text.replace(znak, znak.upper())
+        print(res)
+
+
+except Exception as ex:
+    print(ex)
