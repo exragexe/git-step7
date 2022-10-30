3d68ee4071d2baa2848f11b431f813b05e7fb57e
yaros <yaroslavdisiak@gmail.com>
2022-10-30 11:16:50 +0200

    lab 5 done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 
+
+try:
+    text = input("Введіть текст: ")
+    slowo = input("Введіть слово яке ви хочете замінити: ")
+    substitute = input(f"Введіть слово яким ви хочете замінити {slowo}: ")
+
+    res = text.replace(slowo, substitute)
+    if slowo not in text:
+        print("Такого слова немає в тексті")
+    else:
+        print(res)
+
+except Exception as e:
+    print(e)
